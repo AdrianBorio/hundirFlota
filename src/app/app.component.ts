3656fe1dbf4784532703deee7743c51029d32606
Adrian <smxbamunozborio@iesjoanramis.org>
2022-03-09 13:40:44 +0100

    Dos tablas con alert(X,Y)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-
-
   filas = new Array(7).fill(null);
   columnas = new Array(7).fill(null);
 
   title = 'hundirFlota';
+
+  clickCelda(columna: any, fila: any) {
+    fila++;
+    columna++;
+    alert('Columna: ' + columna + '\n' + 'Fila: ' + fila);
+  }
 }
